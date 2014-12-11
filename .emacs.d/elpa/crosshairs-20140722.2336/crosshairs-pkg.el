16f97313f389b5afb36f411b19ee347d505769ea
Daniel Koch <koch@triple6.org>
2014-12-11 16:31:19 +0100

    pkg updates

--- .emacs.d/elpa/crosshairs-20140722.2336/crosshairs-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "crosshairs" "20140722.2336" "Highlight the current line and column." (quote ((hl-line+ "0") (col-highlight "0") (vline "0"))))
---
+++ .emacs.d/elpa/parenface-20141125.828/parenface-autoloads.el
@@ -0,0 +1,16 @@
+;;; parenface-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("parenface-pkg.el" "parenface.el") (21641
+;;;;;;  47240 394679 113000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; parenface-autoloads.el ends here
